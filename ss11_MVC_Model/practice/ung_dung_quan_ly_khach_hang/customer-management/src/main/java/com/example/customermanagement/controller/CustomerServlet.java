a2e466f4e17cb2aad1de890eb43705541bfa2bd0
ngoctangit2504 <lulu25042001@gmail.com>
2023-12-01 00:45:47 +0700

    ss11 update

---
+++ ss11_MVC_Model/practice/ung_dung_quan_ly_khach_hang/customer-management/src/main/java/com/example/customermanagement/controller/CustomerServlet.java
@@ -0,0 +1,84 @@
+package com.example.customermanagement.controller;
+
+import com.example.customermanagement.model.Customer;
+import com.example.customermanagement.service.ICustomerService;
+import com.example.customermanagement.service.impl.CustomerService;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "CustomerServlet", value = "/customer")
+public class CustomerServlet extends HttpServlet {
+
+    private ICustomerService customerService = new CustomerService();
+
+    // Hien thi danh sach
+    private void listCustomers(HttpServletRequest request, HttpServletResponse response) {
+        List<Customer> customers = this.customerService.findAll();
+        request.setAttribute("customers", customers);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("customer/list.jsp");
+        try {
+            dispatcher.forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Them khach hang
+    private void showCreateForm(HttpServletRequest request, HttpServletResponse response) {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("customer/create.jsp");
+        try {
+            dispatcher.forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if(action == null){
+            action = "";
+        }
+        switch (action){
+            case "create":
+                showCreateForm(request, response);
+                break;
+            case "edit":
+                break;
+            case "delete":
+                break;
+            case "view":
+                break;
+            default:
+                listCustomers(request, response);
+                break;
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action){
+            case "create":
+                break;
+            case "edit":
+                break;
+            case "delete":
+                break;
+            default:
+                break;
+        }
+    }
+}
